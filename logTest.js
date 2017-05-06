4872dda39412a44680db9698c45a1a45480455c0
Vincent van Beek <vin.van.beek@outlook.com>
2017-05-06 23:44:32 +0200

    news endpoint removed and renamed module to 'logtest'

--- logTest.js
+++ logTest.js
@@ -1,12 +1,15 @@
 /* --------------------------------------------------------------------------------------------------
    logTest - logging example.
-   - using bunyan as logging framework
+   - using bunyan as logging framework 
+   - using MongoDB as database
+   - using express to build a REST Webservice API
    2017-05-03 Vincent van Beek
 ----------------------------------------------------------------------------------------------------- */
 "use strict";
 var express = require("express");           // Express web application framework. http://expressjs.com/
 var mongoose = require("mongoose");         // Framework to access MongoDB (Database) using JSON syntax.
-var bodyParser = require("body-parser");    // Parse incoming request bodies in a middleware before your handlers, available under the req.body property. See https://github.com/expressjs/body-parser
+var bodyParser = require("body-parser");    // Parse incoming request bodies in a middleware before your handlers, 
+                                            // available under the req.body property. See https://github.com/expressjs/body-parser
 
 var APPNAME = "logTest";                    // Name of this app used here and there
 var PORT = process.env.PORT || 8088;        // Node will listen on port from environment setting, or when not set to port number...
@@ -21,29 +24,24 @@ var log = bunyan.createLogger({
     src: true,                              // show source filename, function and line number
                                             // Note: set to false in the production environment since it is time consuming
     streams: [{
-        type: "rotating-file",              // Keep up to 7 log files (each day one log file)
-        path: "./logs/logTest.log",
+        type: "rotating-file",              // Log files will "roll" over (automatic deletion after configured period)
+        path: "./logs/logTest.log",         // Path and filename of the log file
         period: "1d",                       // daily rotation
-        count: 30                           // keep 30 copies (1 month)
+        count: 30                           // keep 31 copies / files (~1 month)
     }],
-    serializers: {
-        req: bunyan.stdSerializers.req,      // Bunyan's HTTP server request serializer with a suggested set of keys.
-        res: bunyan.stdSerializers.res       // Bunyan's HTTP server response serializer with a suggested set of keys.
+    serializers: {                          // usage of default serializers:
+        req: bunyan.stdSerializers.req,     // Bunyan's HTTP server request serializer with a suggested set of keys.
+        res: bunyan.stdSerializers.res      // Bunyan's HTTP server response serializer with a suggested set of keys.
     }
 });
 
 
-// Connect with database 'bookAPI' on MongoDB
-var db = mongoose.connect('mongodb://localhost/bookAPI'); // use local database named: 'bookAPI'
-var Book = require('./models/bookModel');                 // our 'book' record structure
+// Connect with database 'bookAPI' on MongoDB and use local database named: 'bookAPI'
+var db = mongoose.connect('mongodb://localhost/bookAPI'); 
+var Book = require('./models/bookModel');   // our 'book' record structure
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());                 // find json object (in url) and make available in req.body
-
-
-// static link the www-root folder to a 'html' directory (located in the users home directory)
-app.use('/', express.static('/home/vincent/html'));       // use your own user / home directory here !
-log.info('www-root linked to folder /home/vincent.html');
 
 
 // CORS: Allow cross-domain requests (blocked by default)
@@ -61,28 +59,20 @@ app.use(function (req, res, next) {
 });
 
 
-// use domain-name/api/books as main end point for "books"
+// static link 'html' directory 
+app.use('/', express.static('/home/vincent/html'));       // use your own user / home directory here ! TODO: use ConfigFile for this.
+log.info('www-root linked to folder /home/vincent.html');
+
+
+// REST WebService API for a 'Book'. 
+// Using this API a book can be created, read, updated and deleted (CRUD) from the database.
+// 'domain-name/api/books' ia the end point for "books"
 var bookRouter = require('./routes/bookRoutes')(Book);
 app.use('/api/books', bookRouter);
-//app.use('/api/authors', authorRouter);
+//app.use('/api/authors', authorRouter);  // TODO add authors as well ; and make a relation.
 
 
-app.get("/news", function (req, res) {
-    try {
-        req.log.info("NEWS");
-        req.log.info({req: req}, "received request");
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<p>No news Today</p><br>");
-        res.end();
-    } catch (err) {
-        req.log.error("/news/ : Something went wrong....", err);
-        res.status(503, "Something went wrong: " + err).end();
-    }
-    req.log.info({res: res}, "responded");
-});
-
-
-// Start web-server and Log that we have started and accept incomming connections on the configured port/
+// Start web-server and Log that we have started and accept incomming connections on the configured port.
 app.listen(PORT, function () {
     log.info(APPNAME + " is ready and listening on port: " + PORT);
     console.log(APPNAME + " is ready and listening on port: " + PORT);
