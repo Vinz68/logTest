f6bd1265e381af2d7479dc5b3a1148e395c2df96
Vincent van Beek <vin.van.beek@outlook.com>
2017-05-26 20:35:41 +0200

    as example

--- tests/bookIntegrationTests.js
+++ tests/bookIntegrationTests.js
@@ -8,20 +8,22 @@ var should = require('should'),
     request = require('supertest'),
     app = ('../logTest.js'),
     mongoose = require("mongoose"),
-    //Book = mongoose.model('Book'),
-    Book = require('../models/bookModel').Book;   // our 'book' record structure
+    Book = mongoose.model('Book')
+    //Book = require('../models/bookModel').Book;   // our 'book' record structure
+
+const agent = request.agent(app);
 
 describe('Book Crud Tests', function(){
     it('Should allow a book to be posted and return a read and _id', function(done){
         var bookPost = {title:'new Book', authors:'Vincent', genre:'Fiction'};
 
-        request(app)
+        agent
             .post('/api/books')
             .send(bookPost)
             .expect(201)
-            .end(function(err, results){
-                results.body.read.should.equal(false);
-                results.body.should.have.property('_id');
+            .end(function(err, res){
+                res.body.read.should.equal(false);
+                res.body.should.have.property('_id');
                 done()
             })
     })
